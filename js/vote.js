321f871df593bc97b7056ff53dd69c034f56b447
bst-07 <elomraniabdelbasset0@gmail.com>
2026-02-07 21:14:00 +0100

    Update vote.js

--- js/vote.js
+++ js/vote.js
@@ -39,12 +39,12 @@ function updateIconFill(){
   if(userVote === 'likes'){
     likeIcon.setAttribute('fill','currentColor');  
     dislikeIcon.setAttribute('fill','none');   
-    likeIcon.style.backgroundColor = '#7dc597';
+
 
   } else if(userVote === 'dislikes'){
     dislikeIcon.setAttribute('fill','currentColor');
     likeIcon.setAttribute('fill','none');
-    dislikeIcon.style.backgroundColor = '#eb8d8d';
+
   } else {
     likeIcon.setAttribute('fill','none');
     dislikeIcon.setAttribute('fill','none');
